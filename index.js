85de7c4ba276df487a4a86b69ae54d8468a45c29
Vanessa Power <vanessa.l.power@gmail.com>
2022-08-23 22:05:41 -0700

    writing the summing from range function in recursion

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
-function sum(fromN, toN) {
+const sum = (fromN, toN) => {
   // Sum all the values from fromN up to toN
-}
+  if (fromN > toN) return 0;
+  return fromN + sum(fromN + 1, toN);
+};
+
+console.log(sum(3, 7));
 
 module.exports = sum;
